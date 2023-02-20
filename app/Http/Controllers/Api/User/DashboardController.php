df038a1daa932be9bcd3a3895bad0fbc04b34cd6
Partha <partha.beas@gmail.com>
2023-02-20 14:48:15 +0530

    file upload

--- app/Http/Controllers/Api/User/DashboardController.php
+++ app/Http/Controllers/Api/User/DashboardController.php
@@ -31,17 +31,7 @@ class DashboardController extends Controller
     */
    public function userDashboard(Request $request)
    {
-   	 	// $quote = DB::table('orders')
-      //       ->leftjoin('quotes','orders.rfq_no','quotes.rfq_no')
-      //       ->leftjoin('users','quotes.user_id','users.id')
-      //       ->leftjoin('products','quotes.product_id','products.id')
-      //       ->leftjoin('categorys','quotes.cat_id','categorys.id')
-      //       // ->leftjoin('sub_categorys','categorys.id','sub_categorys.cat_id')
-      //       ->select('quotes.rfq_no','quotes.user_id','quotes.id as qid','products.slug','products.status','categorys.*','users.id','users.name as uname','products.id as pid','categorys.id as cid','quotes.quantity','orders.letterhead','orders.po_no','orders.po_date')
-      //       ->orderBy('quotes.updated_at','desc')
-      //       ->where('orders.po_no',$id)
-      //       ->whereNull('quotes.deleted_at')
-      //       ->get()->toArray();
+   	  
    		$userid = $request->user_id;
 
    		$getuser = User::where('id',$userid)->first();
@@ -58,20 +48,21 @@ class DashboardController extends Controller
             ->count();
 	        $data['total_no_of_orders'] = $quote;
 
+	         $rfqNego = DB::table('quotes') 
+	            ->where('quotes.user_id',$userid)
+	            ->where('quotes.kam_status',6)
+	            ->whereNull('quotes.deleted_at')
+	            ->count();
+	        $data['rfq_under_negotiation'] = $rfqNego;
+
 	        $orderCon = DB::table('orders')
 	            ->leftjoin('quotes','orders.rfq_no','quotes.rfq_no')             
 	            ->where('quotes.user_id',$userid)
 	            ->where('orders.status',1)
 	            ->whereNull('quotes.deleted_at')
 	            ->count();
-	        $data['orders_confirmed_&_pending_for_delivery'] = $orderCon;
-
-	        $rfqNego = DB::table('quotes') 
-	            ->where('quotes.user_id',$userid)
-	            ->where('quotes.kam_status',6)
-	            ->whereNull('quotes.deleted_at')
-	            ->count();
-	        $data['rfq_under_negotiation'] = $rfqNego;
+	        $data['orders_confirmed_&_pending_for_delivery'] = $orderCon; 
+	       
 
 	        $custComplain = DB::table('complain_main') 
 	            ->where('complain_main.user_id',$userid)
@@ -91,21 +82,24 @@ class DashboardController extends Controller
 	        // dd($getuser->zone);
 	        $orderCon = DB::table('orders')
 	            ->leftjoin('quotes','orders.rfq_no','quotes.rfq_no')             
-	            ->where('quotes.user_id',$userid)
+	            ->leftjoin('users','quotes.user_id','users.id')
 	            ->where('orders.status',1)
+	            ->where('users.zone',$getuser->zone)
 	            ->whereNull('quotes.deleted_at')
 	            ->count();
 	        $data['orders_confirmed_&_pending_for_delivery'] = $orderCon;
 
 	        $rfqNego = DB::table('quotes') 
-	            ->where('quotes.user_id',$userid)
+	            ->leftjoin('users','quotes.user_id','users.id')
 	            ->where('quotes.kam_status',6)
+	            ->where('users.zone',$getuser->zone)
 	            ->whereNull('quotes.deleted_at')
 	            ->count();
 	        $data['rfq_under_negotiation'] = $rfqNego;
 
 	        $custComplain = DB::table('complain_main') 
-	            ->where('complain_main.user_id',$userid)
+	        	->leftjoin('users','complain_main.user_id','users.id')
+	            ->where('users.zone',$getuser->zone)
 	            ->where('complain_main.closed_status',1) 
 	            ->count();
 
